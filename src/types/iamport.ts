d9152092b622e09799fee472aebcdfd67ebfe12e
sky17892 <46701383+sky17892@users.noreply.github.com>
2025-04-29 08:45:10 +0900

    Update and rename toss.d.ts to iamport.ts

---
+++ src/types/iamport.ts
@@ -0,0 +1,26 @@
+declare global {
+    interface Window {
+      IMP: {
+        init: (accountId: string) => void;
+        request_pay: (params: IamportPaymentOptions, callback: (rsp: any) => void) => void;
+      };
+    }
+  }
+  
+  /**
+   * 아임포트 결제 요청 파라미터 타입
+   */
+  export interface IamportPaymentOptions {
+    pg: 'tosspayments'; // 결제사 선택
+    pay_method: 'card'; // 카드 결제만 사용
+    merchant_uid: string; // 주문 고유 번호
+    name: string; // 주문명
+    amount: number; // 결제 금액 (원)
+    buyer_email?: string;
+    buyer_name?: string;
+    buyer_tel?: string;
+    buyer_addr?: string;
+    buyer_postcode?: string;
+    m_redirect_url?: string; // 결제 완료 후 리다이렉트 URL
+  }
+  
