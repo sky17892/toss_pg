7412a6c9f53e016d3bdb261ecf19bdbcaf6c837b
sky17892 <46701383+sky17892@users.noreply.github.com>
2025-08-21 11:18:23 +0900

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -62,7 +62,7 @@ export function initHomePage(): void {
     const redirectBaseUrl = 'https://gurumauto.cafe24.com/myshop/order/list.html';
 
     const paymentData: RequestPayment = {
-      pg: 'html5_inicis.MOI0559698', // ✅ 실결제 MID
+      pg: 'html5_inicis', // ✅ 실결제 MID
       pay_method: 'card',
       merchant_uid: orderId,
       name,
